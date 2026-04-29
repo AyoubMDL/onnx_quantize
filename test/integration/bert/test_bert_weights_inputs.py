bbb2790a3c79db160cc7928727a992876a219256
AyoubMDL <ayoubbih119@gmail.com>
2026-04-29 19:20:02 +0200

    test(integration): use approx

--- test/integration/bert/test_bert_weights_inputs.py
+++ test/integration/bert/test_bert_weights_inputs.py
@@ -65,4 +65,4 @@ def test_quantize_bert_weights_inputs(
         correct += (preds == batch["label"]).sum().item()
 
     # float has ~0.94 accuracy
-    assert correct / len(bert_dataset) == expected_accuracy
+    assert correct / len(bert_dataset) == pytest.approx(expected_accuracy, abs=0.02)
